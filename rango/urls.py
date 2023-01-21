5085d67e2bbd3090e5c4179f28c099e63fdde835
Morvern Mackintosh <2809522m@student.gla.ac.uk>
2023-01-21 21:59:37 +0000

    Chapter 3 created a view and rango app

---
+++ rango/urls.py
@@ -0,0 +1,11 @@
+# importing url mapping machinery
+from django.urls import path
+# importing views module from rango app
+from rango import views
+
+app_name = 'rango'
+# python list[]
+urlpatterns = [
+    # path function - first parameter is string of URL, second is the view that will be called if a match is found, third is name
+    path('',views.index, name= 'index')
+]
